a281ac74d71e8be1d1178bf87970c4447e362b09
Daniel Deverell <daniel.deverell@gmail.com>
2022-09-13 21:24:50 -0400

    final class one

--- app/scripts.js
+++ app/scripts.js
@@ -1,14 +1,17 @@
 // select document elements & store them in variables
-var textSpan = document.querySelector("p span");
-var kittenPic = document.querySelector("img");
+const textSpan = document.querySelector("p span");
+const kittenPic = document.querySelector("img");
 
 // create a new variable and initialize it to zero (stores kitten clicks)
-var kittenClicks = 0;
+let kittenClicks = 0;
 
 // add a click event listener to kittenPic
-kittenPic.addEventListener("click", function () {
+kittenPic.addEventListener("click", registerEvent);
+
+function registerEvent() {
   // commands to run when the kitten is clicked
-  kittenClicks = kittenClicks + 1;
-  console.log("You kitten clicked " + kittenClicks + " times!");
-  textSpan.textContent = kittenClicks;
-});
+  kittenClicks++;
+  // console.log("You kitten clicked " + kittenClicks + " times.");
+  console.log(`You kitten clicked ${kittenClicks} times!`);
+  textSpan.innerText = kittenClicks;
+}
